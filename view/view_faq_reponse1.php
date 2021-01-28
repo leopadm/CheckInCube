517bc0e6b71699a2310e1a538896e0239ef5af0a
flozmr <florian.zimmer@laposte.net>
2021-01-28 22:12:56 +0100

    commit

---
+++ view/view_faq_reponse1.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<link rel="icon" type="image/png" href="CerveauAPP.png">
+	<title>PsychoTech Industries</title>
+	<link rel="stylesheet" type="text/css" href="FAQ.css">
+	<link rel="preconnect" href="https://fonts.gstatic.com">
+	<link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+
+</head>
+
+<body>
+	
+	<nav class="questions">
+    <ul>
+      <li class="deroulant1"><a href="#">Questions</a>
+          <ul class="sous1">
+            <li><a href="view_faq_reponse1.php">Qui êtes-vous ?</a></li>
+            <li><a href="view_faq_reponse2.php">Est il possible de consulter mes informations personnelles ?</a></li>
+            <li><a href="view_faq_reponse3.php">Est il possible de voir ses Rendez-vous prévus ?</a></li>
+            <li><a href="view_faq_reponse4.php">Où est il possible de voir ses résultats ?</a></li>
+            <li><a href="view_faq_reponse5.php">Est il possible de prendre un rendez-vous ?</a></li>
+            <li><a href="view_faq_reponse6.php">Comment modifier ses données personnelles ?</a></li>
+            <li><a href="view_faq_reponse7.php">Comment changer la langue ?</a></li>
+          </ul>
+      </li>
+    </ul>
+
+	<div id="faq">
+		<H1>Foire aux questions</H1>
+	</div>
+
+  <div id="reponse">
+    <h2>Qui êtes-vous ?</h2>
+    <p>Psychotech est une société de solution numérique, sa solution CheckInCube permet d'aider les pilotes pour leur tests psychotechnique.</p>
+  </div>
+
+	
+</body>
